cc604d51ab9ff3db4a77a2026d01185ae8dd32cf
Surabordin <bluelimec@gmail.com>
2022-02-18 16:26:13 +0700

    complete

--- lab26_3.cpp
+++ lab26_3.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
+#include<set>
 
 using namespace std;
 
-//Write function count() here.
+
+int count(int a[],int b){
+	set<int> myset;
+	for (int i = 0 ; i < b ; i++){
+		myset.insert(a[i]);
+	}
+	return myset.size();
+}
+
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
